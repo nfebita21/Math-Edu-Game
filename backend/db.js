9e6f046379aa29ea415db73a105c30aaaee1b611
nadyadf <nadyadf3@gmail.com>
2026-04-10 20:32:15 +0700

    update name of password key in .env file

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   connectionLimit: 10
