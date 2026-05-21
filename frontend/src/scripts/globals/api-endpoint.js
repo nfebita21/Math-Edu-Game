f2f0772ac7245bdc777d0f886f4de8eaf74aa5e7
nadyadf <nadyadf3@gmail.com>
2026-05-21 11:59:59 +0700

    change database

--- frontend/src/scripts/globals/api-endpoint.js
+++ frontend/src/scripts/globals/api-endpoint.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://math-edu-game-production.up.railway.app';
+const BASE_URL = 'http://industrious-adventure-production-0c43.up.railway.app';
 
 const API_ENDPOINT = {
   students: `${BASE_URL}/students`,
