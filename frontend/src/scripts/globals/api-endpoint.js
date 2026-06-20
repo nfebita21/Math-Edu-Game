a0de6af57d2668dffb1832f00bb1494ef225bff8
nadyadf <nadyadf3@gmail.com>
2026-06-20 15:21:13 +0700

    add s to url http

--- frontend/src/scripts/globals/api-endpoint.js
+++ frontend/src/scripts/globals/api-endpoint.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://math-edu-game-production-9a56.up.railway.app';
+const BASE_URL = 'https://math-edu-game-production-9a56.up.railway.app';
 
 const API_ENDPOINT = {
   students: `${BASE_URL}/students`,
